25b3aac942e333e748ab051231548430fc0faa3c
Kristian Urup Olesen Larsen <kristianuruplarsen@gmail.com>
2018-08-02 14:07:03 +0200

    Revert "fail"
    This reverts commit 8e1bc9b01134e627960985f4742b73a7e6d833b5.

--- examples.py
+++ examples.py
@@ -1,28 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import cmocean
 
-from pretty.pretty import *
-
-
-style = pretty('totoro')
-
-
-style.palette = 'ponyo'
-
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-plt.plot(np.linspace(0,10), np.sqrt(np.linspace(0,10)))
-plt.show()
-
-
+from pretty.pretty import style_classic, style_modern
 
 # ------------------------------------------------------------------------------
 # Example 1: sigmoid lines
-style.style_classic()
+style_classic('ponyo')
 
 def sigmoid(z, c):
     return c/(1 + np.exp(-z))
@@ -59,27 +42,4 @@ plt.xlabel('x')
 plt.ylabel(r"$y = 3x + \varepsilon$")
 plt.title('Heterogeneous noise')
 plt.savefig('example_figures/hetero.png')
-plt.show()
-
-
-
-# ------------------------------------------------------------------------------
-# Example 3: continous variable
-from matplotlib.colors import LinearSegmentedColormap
-
-cm = LinearSegmentedColormap.from_list('test', ["#5f8bcc","#3069c2","#2f4337","#4e693c","#77a66f","#8f3535","#a7784e","#985d32","#e2d234","#d9db6e"], N=100)
-
-x = y = np.linspace(0,10, 100)
-
-def f(x,y):
-    return np.sqrt(x*y)
-
-X,Y = np.meshgrid(x, y)
-Z = np.zeros(X.shape)
-
-for i in range(X.shape[0]):
-    for j in range(X.shape[0]):
-        Z[i,j] = f(X[i,j], Y[i,j])
-
-plt.pcolor(X,Y,Z, cmap = cm)
 plt.show()
